ae5b28df809f408a781d6d62502bb6b2a0d15d4c
Lucas <lucaswong.ca@gmail.com>
2020-09-27 16:40:58 -0700

    Refactor app data into a custom hook:

--- client/src/Components/App/App.jsx
+++ client/src/Components/App/App.jsx
@@ -1,57 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import ExpenseList from "../ExpenseList/ExpenseList";
 import InputExpense from "../InputExpense/InputExpense";
 import Header from "../Header/Header";
 
+import useApplicationData from "../../hooks/useApplicationData";
+
 export default function App() {
-  const [expenses, setExpenses] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:9000/api/expenses").then((res) => {
-      setExpenses(res.data);
-    });
-  }, []);
-
-  const addExpense = (name, cost, category) => {
-    if (name === "" || cost === "" || category === "") return;
-    return axios
-      .post("http://localhost:9000/api/expenses/new", {
-        name,
-        cost,
-        category
-      })
-      .then((res) => {
-        const newExpenses = [
-          ...expenses,
-          {
-            id: res.data.id,
-            name: name,
-            cost: cost,
-            category: category
-          }
-        ];
-        setExpenses(newExpenses);
-      });
-  };
-
-  const deleteExpense = (id) => {
-    return axios
-      .post("http://localhost:9000/api/expenses/delete", {
-        id: id
-      })
-      .then((res) => {
-        const filter = [...expenses].filter((expense) => {
-          if (expense.id === id) {
-            return null;
-          } else {
-            return expense;
-          }
-        });
-        setExpenses(filter);
-      });
-  };
+  const { expenses, addExpense, deleteExpense } = useApplicationData();
 
   return (
     <section className="hero is-fullheight">
---
+++ client/src/hooks/useApplicationData.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useApplicationData = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:9000/api/expenses").then((res) => {
+      setExpenses(res.data);
+    });
+  }, []);
+
+  const addExpense = (name, cost, category) => {
+    if (name === "" || cost === "" || category === "") return;
+    return axios
+      .post("http://localhost:9000/api/expenses/new", {
+        name,
+        cost,
+        category
+      })
+      .then((res) => {
+        const newExpenses = [
+          ...expenses,
+          {
+            id: res.data.id,
+            name: name,
+            cost: cost,
+            category: category
+          }
+        ];
+        setExpenses(newExpenses);
+      });
+  };
+
+  const deleteExpense = (id) => {
+    return axios
+      .post("http://localhost:9000/api/expenses/delete", {
+        id: id
+      })
+      .then((res) => {
+        const filter = [...expenses].filter((expense) => {
+          if (expense.id === id) {
+            return null;
+          } else {
+            return expense;
+          }
+        });
+        setExpenses(filter);
+      });
+  };
+  return { expenses, addExpense, deleteExpense };
+};
+
+export default useApplicationData;
